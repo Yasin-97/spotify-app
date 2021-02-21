ec4db41a72801a8f636c6df0a9d4f77b60341f51
Yasin Zahiri <yasin.zahiri10@gmail.com>
2021-02-21 21:48:01 +0330

    Add files via upload

---
+++ src/component/player/bigDevice/BigDevice.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './bigDevice.css'
+import spotifyLogo from '../../../img/asit.png'
+function BigDevice() {
+  return (
+    <div class="big__device">
+      <img src={spotifyLogo} alt="" />
+      <h3>Please open app on a desktop device.</h3>
+    </div>
+  );
+}
+
+export default BigDevice;
